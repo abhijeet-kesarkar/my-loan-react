13b9bc0e3cba44dea1f747b77af2d742e717ef66
Abhijeet Kesarkar <abhijit.kesar@gmail.com>
2017-07-22 20:16:25 +0530

    added reactstrap

---
+++ src/components/layout/ApplicationContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import LoanApplicationWizard from './LoanApplicationWizard';
+
+class ApplicationContainer extends Component {
+  render() {
+    return (
+        <LoanApplicationWizard></LoanApplicationWizard>
+    );
+  }
+}
+
+export default ApplicationContainer;

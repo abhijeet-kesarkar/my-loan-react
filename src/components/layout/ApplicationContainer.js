921d72009021fadcfa4cd969efbce044a425941a
Abhijeet Kesarkar <abhijit.kesar@gmail.com>
2017-07-22 22:41:32 +0530

    Added bootstrap

--- src/components/layout/ApplicationContainer.js
+++ src/components/layout/ApplicationContainer.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import LoanApplicationWizard from './LoanApplicationWizard';
+import { Container, Row, Col } from 'reactstrap';
 
 class ApplicationContainer extends Component {
-  render() {
-    return (
-        <LoanApplicationWizard></LoanApplicationWizard>
-    );
-  }
+    render() {
+        return ( 
+        <Container>
+        <Row>
+          <Col>
+          < LoanApplicationWizard > </LoanApplicationWizard>
+          </Col>
+        </Row>
+        </Container>
+        );
+    }
 }
 
 export default ApplicationContainer;
